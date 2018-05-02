6b9b19fd3e529f994fbfe73a9bce78f8ab7d7dfa
marcocaballero <marco.caballero.d@gmail.com>
2018-05-02 02:38:03 +0200

    Commit about asintotic notation eficient

--- src/main/java/io/github/marcocaballero/Main.java
+++ src/main/java/io/github/marcocaballero/Main.java
@@ -3,12 +3,12 @@ package io.github.marcocaballero;
 public class Main {
 
 	public static void main(String[] args) {
-//		int n = 8;
-//		int n = 16;
-//		int n = 64;
+		// int n = 8;
+		// int n = 16;
+		// int n = 64;
 		int n = 7;
-//		int n = 15;
-//		int n = 31;
+		// int n = 15;
+		// int n = 31;
 
 		Algorithm.printResultsAsTable(BruteForceScheduler.generateResults(n));
 		// Algorithm.printResultsAsCalendar(BruteForceScheduler.generateResults(n));
